16fb20b761adeb7e2198b06b0c3de9028f958e89
Chandan <chandan.kumar.dash@cachatto.com>
2025-12-26 10:10:34 +0530

    Initial package setup with iOS 13+ configuration.

--- Package.swift
+++ Package.swift
@@ -1,8 +1,25 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "ScreenShield"
+    name: "ScreenShield",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        // The library product that will be available to consumers of this package.
+        .library(
+            name: "ScreenShield",
+            targets: ["ScreenShield"]
+        )
+    ],
+    targets: [
+        // The main target containing all source files.
+        .target(
+            name: "ScreenShield",
+            path: "Sources/ScreenShield"
+        )
+    ]
 )
